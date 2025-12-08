88b2b1e31a03172dc36f6997a8207410242a819f
glassdekko <glassdekko@gmail.com>
2025-12-08 22:27:12 +0900

    feat:[김유리] Google OAuth 로그인 및 로그아웃 기능 구현 [SOTS-1]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from config.database.session import Base, engine
 from samsam_board.adapter.input.web.samsam_board_router import samsam_board_router
 from samsam_naver.adapter.naver_router import router as naver_router
-
+from social_oauth.adapter.input.web.google_oauth2_router import authentication_router
 load_dotenv()
 
 app = FastAPI()
@@ -27,9 +27,12 @@ app.add_middleware(
 # Router 등록
 app.include_router(naver_router, prefix="/market-data")
 app.include_router(samsam_board_router, prefix="/board")
+app.include_router(authentication_router, prefix="/authentication")
 
 
 if __name__ == "__main__":
     import uvicorn
+    host = os.getenv("APP_HOST")
+    port = int(os.getenv("APP_PORT"))
     Base.metadata.create_all(bind=engine)
     uvicorn.run(app, host="0.0.0.0", port=33333)
